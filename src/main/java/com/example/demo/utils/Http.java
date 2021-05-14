a40a8d2218145d32f71cf2cca6515211d8c7884e
diydriller <dhrhd080@naver.com>
2021-05-14 18:56:10 +0900

    kakao refactoring

---
+++ src/main/java/com/example/demo/utils/Http.java
@@ -0,0 +1,43 @@
+package com.example.demo.utils;
+
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+public class Http {
+
+    public static ResponseEntity<String> getHttp(String url, HttpEntity entity){
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(3000);
+        HttpClient httpClient = HttpClientBuilder.create()
+                .setMaxConnTotal(100)
+                .setMaxConnPerRoute(5)
+                .build();
+        factory.setHttpClient(httpClient);
+        RestTemplate restTemplate = new RestTemplate(factory);
+
+        return restTemplate.exchange(url, HttpMethod.GET,entity, String.class);
+    }
+
+    public static ResponseEntity<String> postHttp(String url, HttpEntity entity){
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(3000);
+        HttpClient httpClient = HttpClientBuilder.create()
+                .setMaxConnTotal(100)
+                .setMaxConnPerRoute(5)
+                .build();
+        factory.setHttpClient(httpClient);
+        RestTemplate restTemplate = new RestTemplate(factory);
+
+        return restTemplate.exchange(url, HttpMethod.POST,entity, String.class);
+    }
+
+
+}
